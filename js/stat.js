ca3112b86ff87c334f2f0edc52ad0cdb56415fce
kornienko199004 <kornienko19904@gmail.com>
2017-11-22 22:05:19 +0300

    Отработка замечаний - CANVAS

--- js/stat.js
+++ js/stat.js
@@ -1,69 +1,67 @@
 'use strict';
 
 window.renderStatistics = function (ctx, names, times) {
+  var HISTOGRAM_WIDTH = 150;
+  var HISTOGRAM_BAR_WIDTH = 40;
+  var HISTOGRAM_INDENT = 50 + HISTOGRAM_BAR_WIDTH;
+  var HISTOGRAM_INITIAL_X = 120;
+  var HISTOGRAM_INITIAL_Y = 95;
+
   var returnWorstTime = function (timesArray) {
-    var max = -1;
+    var maxTime = -1;
     var maxIndex = -1;
     var lastPlayerData = [];
     for (var i = 0; i < timesArray.length; i++) {
       var time = timesArray[i];
-      if (time > max) {
-        max = Math.floor(time);
+      if (time > maxTime) {
+        maxTime = Math.floor(time);
         maxIndex = i;
       }
     }
-    lastPlayerData.push(max);
+    lastPlayerData.push(maxTime);
     lastPlayerData.push(maxIndex);
     return lastPlayerData;
   };
 
-  var drawHistograma = function (canvasObject, i, time, oneStep, name) {
-    var currentY;
+  var drawHistogram = function (canvasContext, i, time, oneStep, name) {
+    var currentY = HISTOGRAM_WIDTH - time * oneStep;
     var opacity;
-    var timeFloor;
-    timeFloor = Math.floor(time);
-    currentY = HISTOGRAMA_WIDTH - time * oneStep;
-    canvasObject.fillText(timeFloor, HISTOGRAMA_INITIAL_X + HISTOGRAMA_INDENT * i, HISTOGRAMA_INITIAL_Y - HISTOGRAMA_BAR_WIDTH / 4);
+    var timeFloor = Math.floor(time);
+    canvasContext.fillText(timeFloor, HISTOGRAM_INITIAL_X + HISTOGRAM_INDENT * i, HISTOGRAM_INITIAL_Y - HISTOGRAM_BAR_WIDTH / 4);
     if (name === 'Вы') {
-      canvasObject.fillStyle = 'rgba(255, 0, 0, 1)';
+      canvasContext.fillStyle = 'rgba(255, 0, 0, 1)';
     } else {
       opacity = Math.ceil(Math.random() * 10) / 10;
-      canvasObject.fillStyle = 'rgba(0, 0, 255,' + opacity + ')';
+      canvasContext.fillStyle = 'rgba(0, 0, 255,' + opacity + ')';
     }
-    canvasObject.fillRect(HISTOGRAMA_INITIAL_X + HISTOGRAMA_INDENT * i, HISTOGRAMA_INITIAL_Y + currentY, HISTOGRAMA_BAR_WIDTH, timeFloor * oneStep);
-    canvasObject.fillStyle = '#000';
-    canvasObject.fillText(name, HISTOGRAMA_INITIAL_X + HISTOGRAMA_INDENT * i, HISTOGRAMA_INITIAL_Y + HISTOGRAMA_WIDTH + HISTOGRAMA_BAR_WIDTH / 3);
+    canvasContext.fillRect(HISTOGRAM_INITIAL_X + HISTOGRAM_INDENT * i, HISTOGRAM_INITIAL_Y + currentY, HISTOGRAM_BAR_WIDTH, timeFloor * oneStep);
+    canvasContext.fillStyle = '#000';
+    canvasContext.fillText(name, HISTOGRAM_INITIAL_X + HISTOGRAM_INDENT * i, HISTOGRAM_INITIAL_Y + HISTOGRAM_WIDTH + HISTOGRAM_BAR_WIDTH / 3);
   };
 
-  var drawCloud = function (canvasObject, startX, startY, width, height, color) {
-    canvasObject.fillStyle = color;
-    canvasObject.fillRect(startX, startY, width, height);
+  var drawCloud = function (canvasContext, startX, startY, width, height, color) {
+    canvasContext.fillStyle = color;
+    canvasContext.fillRect(startX, startY, width, height);
   };
 
-  var HISTOGRAMA_WIDTH = 150;
-  var HISTOGRAMA_BAR_WIDTH = 40;
-  var HISTOGRAMA_INDENT = 50 + HISTOGRAMA_BAR_WIDTH;
-  var HISTOGRAMA_INITIAL_X = 120;
-  var HISTOGRAMA_INITIAL_Y = 95;
+  var drawResult = function (canvasContext) {
+    canvasContext.fillStyle = '#000';
+    canvasContext.font = '14px PT Mono';
+    canvasContext.fillText('Ура вы победили!\nСписок результатов:', 120, 40);
+    canvasContext.fillText('Худшее время: ' + lastPlayerTime + 'мс у игрока ' + lastPlayerName, 120, 60);
+  };
 
   var lastPlayerData = returnWorstTime(times);
   var lastPlayerTime = lastPlayerData[0];
   var lastPlayerName = names[lastPlayerData[1]];
-  var step = HISTOGRAMA_WIDTH / (lastPlayerTime - 0);
+  var step = HISTOGRAM_WIDTH / (lastPlayerTime - 0);
 
   drawCloud(ctx, 110, 20, 420, 270, 'rgba(0, 0, 0, 0.7)');
   drawCloud(ctx, 100, 10, 420, 270, 'white');
 
-  ctx.beginPath();
-  ctx.fillStyle = '#000';
-  ctx.font = '14px PT Mono';
-  ctx.fillText('Ура вы победили!\nСписок результатов:', 120, 40);
-
-  ctx.fillText('Худшее время: ' + lastPlayerTime + 'мс у игрока ' + lastPlayerName, 120, 60);
+  drawResult(ctx);
 
   for (var i = 0; i < times.length; i++) {
-    drawHistograma(ctx, i, times[i], step, names[i]);
+    drawHistogram(ctx, i, times[i], step, names[i]);
   }
-
-  ctx.closePath();
 };
