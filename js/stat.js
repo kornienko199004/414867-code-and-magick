d553b111230c1b5a266367493869efbbc0fdb19d
kornienko199004 <kornienko19904@gmail.com>
2017-11-22 21:19:11 +0300

    Отработка замечаний - CANVAS

--- js/stat.js
+++ js/stat.js
@@ -17,43 +17,42 @@ window.renderStatistics = function (ctx, names, times) {
     return lastPlayerData;
   };
 
+  var drawHistograma = function (canvasObject, i, time, initialX, initialY, initialIndent, width, barWidth, oneStep, name) {
+    var currentY;
+    var opacity;
+    var timeFloor;
+    timeFloor = Math.floor(time);
+    currentY = width - time * oneStep;
+    canvasObject.fillText(timeFloor, initialX + initialIndent * i, initialY - barWidth / 4);
+    if (name === 'Вы') {
+      canvasObject.fillStyle = 'rgba(255, 0, 0, 1)';
+    } else {
+      opacity = Math.ceil(Math.random() * 10) / 10;
+      canvasObject.fillStyle = 'rgba(0, 0, 255,' + opacity + ')';
+    }
+    canvasObject.fillRect(initialX + initialIndent * i, initialY + currentY, barWidth, timeFloor * oneStep);
+    canvasObject.fillStyle = '#000';
+    canvasObject.fillText(name, initialX + initialIndent * i, initialY + width + barWidth / 3);
+  };
+
+  var drawCloud = function (canvasObject, startX, startY, width, height, color) {
+    canvasObject.fillStyle = '' + color + '';
+    canvasObject.fillRect(startX, startY, width, height);
+  };
+
   var HISTOGRAMA_WIDTH = 150;
   var HISTOGRAMA_BAR_WIDTH = 40;
   var HISTOGRAMA_INDENT = 50 + HISTOGRAMA_BAR_WIDTH;
   var HISTOGRAMA_INITIAL_X = 120;
   var HISTOGRAMA_INITIAL_Y = 95;
 
-  var timeFloor;
-  var currentY;
-  var opacity;
   var lastPlayerData = returnWorstTime(times);
   var lastPlayerTime = lastPlayerData[0];
   var lastPlayerName = names[lastPlayerData[1]];
   var step = HISTOGRAMA_WIDTH / (lastPlayerTime - 0);
 
-  ctx.beginPath();
-  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-  ctx.moveTo(330, 10);
-  ctx.quadraticCurveTo(80, 10, 60, 120);
-  ctx.quadraticCurveTo(10, 210, 60, 250);
-  ctx.quadraticCurveTo(110, 310, 330, 280);
-  ctx.quadraticCurveTo(510, 290, 510, 240);
-  ctx.quadraticCurveTo(560, 210, 540, 140);
-  ctx.quadraticCurveTo(560, 10, 330, 10);
-  ctx.fill();
-  ctx.closePath();
-
-  ctx.fillStyle = 'white';
-  ctx.beginPath();
-  ctx.moveTo(320, 0);
-  ctx.quadraticCurveTo(70, 0, 50, 110);
-  ctx.quadraticCurveTo(0, 200, 50, 240);
-  ctx.quadraticCurveTo(100, 300, 320, 270);
-  ctx.quadraticCurveTo(500, 280, 500, 230);
-  ctx.quadraticCurveTo(550, 200, 530, 130);
-  ctx.quadraticCurveTo(550, 0, 320, 0);
-  ctx.fill();
-  ctx.closePath();
+  drawCloud(ctx, 110, 20, 420, 270, 'rgba(0, 0, 0, 0.7)');
+  drawCloud(ctx, 100, 10, 420, 270, 'white');
 
   ctx.beginPath();
   ctx.fillStyle = '#000';
@@ -63,18 +62,7 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.fillText('Худшее время: ' + lastPlayerTime + 'мс у игрока ' + lastPlayerName, 120, 60);
 
   for (var i = 0; i < times.length; i++) {
-    timeFloor = Math.floor(times[i]);
-    currentY = HISTOGRAMA_WIDTH - timeFloor * step;
-    ctx.fillText(timeFloor, HISTOGRAMA_INITIAL_X + HISTOGRAMA_INDENT * i, HISTOGRAMA_INITIAL_Y - HISTOGRAMA_BAR_WIDTH / 4);
-    if (names[i] === 'Вы') {
-      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-    } else {
-      opacity = Math.ceil(Math.random() * 10) / 10;
-      ctx.fillStyle = 'rgba(0, 0, 255,' + opacity + ')';
-    }
-    ctx.fillRect(HISTOGRAMA_INITIAL_X + HISTOGRAMA_INDENT * i, HISTOGRAMA_INITIAL_Y + currentY, HISTOGRAMA_BAR_WIDTH, times[i] * step);
-    ctx.fillStyle = '#000';
-    ctx.fillText(names[i], HISTOGRAMA_INITIAL_X + HISTOGRAMA_INDENT * i, HISTOGRAMA_INITIAL_Y + HISTOGRAMA_WIDTH + HISTOGRAMA_BAR_WIDTH / 3);
+    drawHistograma(ctx, i, times[i], HISTOGRAMA_INITIAL_X, HISTOGRAMA_INITIAL_Y, HISTOGRAMA_INDENT, HISTOGRAMA_WIDTH, HISTOGRAMA_BAR_WIDTH, step, names[i]);
   }
 
   ctx.closePath();
